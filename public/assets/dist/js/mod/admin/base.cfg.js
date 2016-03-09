47573d54ebbbc07d8fbe2511367cddaddc856fa7
qijingyu2013 <qijingyu2009@gmail.com>
2016-03-09 16:18:03 +0800

    搭建js框架

---
+++ public/assets/dist/js/mod/admin/base.cfg.js
@@ -0,0 +1,9 @@
+/**
+ * Created by qjy on 16-3-9.
+ */
+seajs.config({
+    base: "../sea-modules/",
+    alias: {
+        "jquery": "jquery/jquery/1.10.1/jquery.js"
+    }
+});
---
+++ public/assets/dist/js/mod/common/base.cfg.js
@@ -0,0 +1,9 @@
+/**
+ * Created by qjy on 16-3-9.
+ */
+seajs.config({
+    base: "../sea-modules/",
+    alias: {
+        "adminsider": "mod/admin/sider.js"
+    }
+});
