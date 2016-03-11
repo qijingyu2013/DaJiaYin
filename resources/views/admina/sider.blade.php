eb4e4f5e78437d7ba8d3ac2de093aed4b45dacda
qijingyu2013 <qijingyu2009@gmail.com>
2016-03-11 10:57:07 +0800

    构建侧边栏

--- resources/views/admina/sider.blade.php
+++ resources/views/admina/sider.blade.php
@@ -5,23 +5,57 @@
             <li class="current"><a href="index.html"><i class="glyphicon glyphicon-home"></i> 控制面板</a></li>
             <li class="submenu current">
                 <a href="#">
-                    <i class="glyphicon glyphicon-list"></i> 首页
+                    <i class="glyphicon glyphicon-list"></i> 关于大家银
+                    <span class="caret pull-right"></span>
+                </a>
+                <!-- Sub menu -->
+                <ul>
+                    <li><a href="calender.html">大家银金属</a></li>
+                    <li><a href="signup.html">奖项及其认证</a></li>
+                    <li><a href="signup.html">大家银优势</a></li>
+                    <li><a href="signup.html">大家银最新公告</a></li>
+                    <li><a href="signup.html">媒体报道</a></li>
+                    <li><a href="signup.html">研发团队</a></li>
+                </ul>
+            </li>
+            <li class="submenu current">
+                <a href="#">
+                    <i class="glyphicon glyphicon-list"></i> 产品中心
+                    <span class="caret pull-right"></span>
+                </a>
+                <ul>
+                    <li><a href="calender.html">金山工业云微盘</a></li>
+                    <li><a href="signup.html">奖项及其认证</a></li>
+                    <li><a href="signup.html">大家银优势</a></li>
+                    <li><a href="signup.html">大家银最新公告</a></li>
+                    <li><a href="signup.html">媒体报道</a></li>
+                    <li><a href="signup.html">研发团队</a></li>
+                </ul>
+            </li>
+            <li class="submenu current">
+                <a href="#">
+                    <i class="glyphicon glyphicon-list"></i> 市场动态
                     <span class="caret pull-right"></span>
                 </a>
                 <!-- Sub menu -->
                 <ul>
                     <li><a href="calender.html">财经日历</a></li>
-                    <li><a href="signup.html">精彩活动</a></li>
-                    <li><a href="signup.html">行情走势</a></li>
-                    <li><a href="signup.html">市场咨询</a></li>
-                    <li><a href="signup.html">视频课程</a></li>
-                    <li><a href="signup.html">专家评论</a></li>
-                    <li><a href="signup.html">公司活动</a></li>
-                    <li><a href="signup.html">研发活动</a></li>
-                    <li><a href="signup.html">荣誉奖项</a></li>
-                    <li><a href="signup.html">合作伙伴</a></li>
+                    <li><a href="calender.html">大家银日刊</a></li>
+                    <li><a href="calender.html">当日点评</a></li>
+                    <li><a href="calender.html">专家点评</a></li>
+                    <li><a href="signup.html">市场最新资讯</a></li>
+                    {{--<li><a href="signup.html">行情走势</a></li>--}}
+                    {{--<li><a href="signup.html">市场咨询</a></li>--}}
+                    {{--<li><a href="signup.html">视频课程</a></li>--}}
+                    {{--<li><a href="signup.html">专家评论</a></li>--}}
+                    {{--<li><a href="signup.html">公司活动</a></li>--}}
+                    {{--<li><a href="signup.html">研发活动</a></li>--}}
+                    {{--<li><a href="signup.html">荣誉奖项</a></li>--}}
+                    {{--<li><a href="signup.html">合作伙伴</a></li>--}}
                 </ul>
             </li>
+
+
         </ul>
     </div>
 </div>
