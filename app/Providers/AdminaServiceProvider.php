0fb96f65637e71c19d69a14495fc2c04674d36c3
qijingyu2013 <qijingyu2009@gmail.com>
2016-03-30 09:56:04 +0800

    百度editor扩展包

--- app/Providers/AdminaServiceProvider.php
+++ app/Providers/AdminaServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use App\Models\Sider;
+//use App\Models\Sider;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -16,8 +16,8 @@ class AdminaServiceProvider extends ServiceProvider
     public function boot()
     {
         //侧边栏
-        $siderLeft = Sider::where( "pid", "=", 0)->with('hasManySiders')->get();
-        view()->share('siderLeft',$siderLeft);
+//        $siderLeft = Sider::where( "pid", "=", 0)->with('hasManySiders')->get();
+//        view()->share('siderLeft',$siderLeft);
     }
 
     /**
